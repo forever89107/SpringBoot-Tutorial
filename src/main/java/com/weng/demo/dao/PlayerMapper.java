24fd4c6ec36dc4a29221438561bb788dc4537ad4
forever89107 <conqueror89107@gmail.com>
2021-12-29 16:40:33 +0800

    MBG完成

--- src/main/java/com/weng/demo/dao/PlayerMapper.java
+++ src/main/java/com/weng/demo/dao/PlayerMapper.java
@@ -1,5 +1,6 @@
 package com.weng.demo.dao;
 
+import com.weng.demo.dao.generate.dao.PlayerDomMapper;
 import com.weng.demo.service.po.Player;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.type.JdbcType;
@@ -9,14 +10,6 @@ import org.apache.ibatis.type.JdbcType;
  */
 
 @Mapper
-public interface PlayerMapper {
-
-    @Select("select * from `player` where id = #{id}")
-    @Results({
-            @Result(column="id", property="id", jdbcType= JdbcType.VARCHAR, id=true),
-            @Result(column="player_name", property="playerName", jdbcType=JdbcType.VARCHAR),
-            @Result(column="job", property="job", jdbcType=JdbcType.VARCHAR),
-            @Result(column="level", property="level", jdbcType=JdbcType.INTEGER)
-    })
-    Player findById(@Param("id") String id);
+public interface PlayerMapper extends PlayerDomMapper {
+    //可自行定義、客製化SQL語法
 }
--- src/main/java/com/weng/demo/service/PlayerService.java
+++ src/main/java/com/weng/demo/service/PlayerService.java
@@ -3,6 +3,8 @@ package com.weng.demo.service;
 import com.weng.demo.common.CommonQuery;
 import com.weng.demo.controller.dto.PlayerDto;
 import com.weng.demo.dao.PlayerMapper;
+import com.weng.demo.dao.generate.po.PlayerDom;
+import com.weng.demo.dao.generate.po.PlayerDomExample;
 import com.weng.demo.res.ResponseException;
 import com.weng.demo.service.po.Player;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -18,8 +21,8 @@ import java.util.List;
 @Service
 public class PlayerService {
 
-    @Autowired
-    private PlayerDAO playerDAO;
+    //    @Autowired
+    //    private PlayerDAO playerDAO;
 
     @Autowired
     private PlayerMapper playerMapper;
@@ -28,17 +31,24 @@ public class PlayerService {
      * 查詢列表
      */
     public List<PlayerDto> getAllPlayer(CommonQuery query) {
-        return playerDAO.select(query);
+        PlayerDomExample example = new PlayerDomExample();
+        example.setOrderByClause(query.getSortBy() + " " + query.getSortRule()); //level desc
+        List<PlayerDom> doms = playerMapper.selectByExample(example);
+
+        // 轉為 DTO
+        List<PlayerDto> result = doms.stream().map(dom -> poToDto(dom)).collect(Collectors.toList());
+        return result;
     }
 
     /**
      * 查詢
      */
     public PlayerDto getPlayerInfo(String id) {
-        Player player = playerMapper.findById(id);
+        PlayerDom player = playerMapper.selectByPrimaryKey(id);
+        if (null == player)
+            throw new ResponseException(HttpStatus.NOT_FOUND.value(), "查無此人");
+
         return poToDto(player);
-//        return playerDAO.select(id).orElseThrow(() ->
-//                new ResponseException(HttpStatus.NOT_FOUND.value(), "查無此人"));
     }
 
     /**
@@ -46,26 +56,39 @@ public class PlayerService {
      */
     public PlayerDto newPlayer(PlayerDto req) {
         // 檢查此Id是否已存在
-        boolean isExistId = playerDAO.select(req.getId()).isPresent();
-        if (isExistId) {
+        PlayerDom dom = playerMapper.selectByPrimaryKey(req.getId());
+        if (null != dom)
             throw new ResponseException(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Id已存在");
-        }
-        // 新增新玩家
-        PlayerDto playerDto = new PlayerDto();
-        playerDto.setId(req.getId());
-        playerDto.setPlayerName(req.getPlayerName());
-        playerDto.setJob(req.getJob());
-        playerDto.setLevel(req.getLevel());
 
-        return playerDAO.insert(playerDto);
+        // 新增玩家成功
+        PlayerDom newDom = new PlayerDom();
+        newDom.setId(req.getId());
+        newDom.setJob(req.getJob());
+        newDom.setPlayerName(req.getPlayerName());
+        newDom.setLevel(req.getLevel());
+
+        if (playerMapper.insert(newDom) > 0)
+            return poToDto(newDom);
+        else
+            throw new ResponseException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error");
     }
 
     /**
      * 編輯
      */
-    public PlayerDto editPlayer(String id, PlayerDto request) {
-        PlayerDto player = getPlayerInfo(id);
-        return playerDAO.update(player.getId(), request);
+    public PlayerDto editPlayer(PlayerDto req) {
+        // 檢查此Id是否有存在
+        PlayerDom dom = playerMapper.selectByPrimaryKey(req.getId());
+        if (null == dom)
+            throw new ResponseException(HttpStatus.NOT_FOUND.value(), "查無此人");
+        // 更新玩家
+        dom.setPlayerName(req.getPlayerName());
+        dom.setJob(req.getJob());
+        dom.setLevel(req.getLevel());
+        if (playerMapper.updateByPrimaryKeySelective(dom) > 0)
+            return poToDto(dom);
+        else
+            throw new ResponseException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error");
     }
 
     /**
@@ -73,13 +96,13 @@ public class PlayerService {
      */
     public Boolean deletePlayer(String id) {
         PlayerDto player = getPlayerInfo(id);
-        return playerDAO.delete(player.getId());
+        return playerMapper.deleteByPrimaryKey(player.getId()) > 0;
     }
 
     /**
      * PO 轉 DTO
      */
-    private PlayerDto poToDto(Player player){
+    private PlayerDto poToDto(PlayerDom player) {
         PlayerDto dto = new PlayerDto();
         dto.setId(player.getId());
         dto.setJob(player.getJob());
